7e039eb69fa8c76a21e6ac7c0618be57d30fd9e9
Piotr Wawrzyńczyk <Wawrzyn321@gmail.com>
2025-06-11 13:18:51 +0200

    Add HTTPS

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const fs = require('fs')
+const https = require('https')
 const { getDisplays, addLikes } = require('./db')
 
-const app = express()
+const PORT = 8097;
+
+const app = express();
 
 app.use(express.static('public'));
 
@@ -16,3 +20,24 @@ app.post('/displays/:id', (req, res) => {
 app.listen(8097, () => {
     console.log('Server listening @ port 8097')
 })
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log("DEV server is listening on port " + PORT);
+  });
+} else {
+  const privateKey = fs.readFileSync("/var/svc/certs/privkey.pem");
+  const certificate = fs.readFileSync("/var/svc/certs/cert.pem");
+
+  https
+    .createServer(
+      {
+        key: privateKey,
+        cert: certificate,
+      },
+      app
+    )
+    .listen(PORT, () => {
+      console.log("PROD server is listening on port " + PORT);
+    });
+}
